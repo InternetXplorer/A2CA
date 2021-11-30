9ec41e37f94b90b4b679d6657a7f95c7664160a9
InternetXplorer <73933562+InternetXplorer@users.noreply.github.com>
2021-11-30 19:27:16 +0100

    added caching of yahoo_fin data to avoid repetition of api calls, and added simple linear regression estimation for EV

--- demo/CCA.py
+++ demo/CCA.py
@@ -1,19 +1,25 @@
 import os
 import sys
 import pandas as pd
+import numpy as np
 import random
+import matplotlib.pyplot as plt
 from recuperation_donnees import get_financial_data
+from recuperation_donnees import calcul_ratios
+from sklearn.linear_model import LinearRegression
 
 os.chdir(sys.path[0])
 
 
 # Récupération des données
-ticker_list = ['F', 'SZKMY', 'NSANY', 'HMC', 'TM', 'STLA'] #secteur de l'automobile (sans respecter de proximité geographique)
-# ticker_list = ['F']
+ticker_list = ['F', 'SZKMY', 'NSANY', 'HMC', 'TM', 'STLA', 'BMW.DE'] #secteur de l'automobile (sans respecter de proximité geographique)
+ticker_european_car_manuf = ['STLA', 'BMW.DE', 'VOW.DE', 'RNO.PA', 'VOLV-A.ST']
+ticker_list = ticker_european_car_manuf
 num_of_comparable_companies = len(ticker_list) - 1
-# num_of_comparable_companies = 5
-studied_comp_ticker = "F"
+studied_comp_ticker = "STLA"
+
 companies_data = get_financial_data(ticker_list)
+
 nbr_of_na = companies_data.isna().sum().sum()
 if (nbr_of_na > 0):
     print(f"{nbr_of_na} missing values. Rows with NaN will be dropped.")
@@ -21,81 +27,36 @@ if (nbr_of_na > 0):
 # print(companies_data.info())
 # print(companies_data)
 
-# companies_data = {}
-# for i in range(0, num_of_comparable_companies+1):
-#     financial_data = {}
-#     financial_data["EV"] = random.randint(10, 50)
-#     financial_data["EBITDA"] = random.randint(1, 12)
-#     financial_data["EBIT"] = random.randint(1, 10)
-#     financial_data["revenue"] = random.randint(1, 50)
-#     financial_data["gross_profit"] = random.randint(3, 20)
-#     financial_data["P_E_ratio"] = random.randint(1, 15)
-#     company_name = ""
-#     if(i == 0):
-#         company_name = studied_comp_name
-#     else:
-#         company_name = "company_" + str(i)
-
-#     companies_data[company_name] = financial_data
-
-
-
-#Calcul des ratios
-# for comp_name in companies_data.keys():
-#     # companies_data[comp_name]["EV_EBITDA_ratio"] = companies_data[comp_name]["EV"] / companies_data[comp_name]["EBITDA"]
-#     companies_data[comp_name]["EV_EBIT_ratio"] = companies_data[comp_name]["EV"] / companies_data[comp_name]["EBIT"]
-#     companies_data[comp_name]["EV_revenue_ratio"] = companies_data[comp_name]["EV"] / companies_data[comp_name]["revenue"]
-#     companies_data[comp_name]["EV_gross_profit_ratio"] = companies_data[comp_name]["EV"] / companies_data[comp_name]["gross_profit"]
-companies_data["EV_EBIT_ratio"] = companies_data["EV"] / companies_data["EBIT"]
-companies_data["EV_revenue_ratio"] = companies_data["EV"] / companies_data["revenue"]
-companies_data["EV_gross_profit_ratio"] = companies_data["EV"] / companies_data["gross_profit"]
-
+companies_data = calcul_ratios(companies_data)
 print(companies_data)
 
 
+#Scatter plot
+stat = "EBIT"
+ax = companies_data.plot.scatter(stat, "EV", title=f"Scatter plot of EV and {stat}")
+
+# Annotate each data point
+for i, txt in enumerate(companies_data.index):
+   ax.annotate(txt, (companies_data[stat].iat[i]+0.05, companies_data["EV"].iat[i]))
+
+
+
 #calcul des moyennes
-# averages = {}
-
-# averages["EV_EBITDA_ratio"] = 0
-# averages["EV_EBIT_ratio"] = 0
-# averages["EV_revenue_ratio"] = 0
-# averages["EV_gross_profit_ratio"] = 0
-# averages["P_E_ratio"] = 0
-
-# for comp_name in companies_data.keys():
-#     if(comp_name != studied_comp_name):
-#         # averages["EV_EBITDA_ratio"] += companies_data[comp_name]["EV_EBITDA_ratio"]
-#         averages["EV_EBIT_ratio"] += companies_data[comp_name]["EV_EBIT_ratio"]
-#         averages["EV_revenue_ratio"] += companies_data[comp_name]["EV_revenue_ratio"]
-#         averages["EV_gross_profit_ratio"] += companies_data[comp_name]["EV_gross_profit_ratio"]
-#         averages["P_E_ratio"] += companies_data[comp_name]["P_E_ratio"]
-
-# # averages["EV_EBITDA_ratio"] /= num_of_comparable_companies
-# averages["EV_EBIT_ratio"] /= num_of_comparable_companies
-# averages["EV_revenue_ratio"] /= num_of_comparable_companies
-# averages["EV_gross_profit_ratio"] /= num_of_comparable_companies
-# averages["P_E_ratio"] /= num_of_comparable_companies
-
-print("\naverages :")
+print("\nAverages :")
 averages = companies_data.mean(axis=0)
 print(averages)
-
-
-
-
 
 #estimation d'EV de l'entreprise étudiée a partir du ratio EV / EBIT
 ratio_to_be_used = "EV_EBIT_ratio"
 denominator_stat = "EBIT"
 estimated_EV = averages.loc[ratio_to_be_used] * companies_data.loc[studied_comp_ticker, denominator_stat]
 
-print("\n\n(EVs are in billion of dollars.)\n")
+print("\n\n(Values are in millions of dollars.)\n")
 print(f"Real EV of {studied_comp_ticker}: {companies_data.loc[studied_comp_ticker, 'EV']}")
 print(f"Estimated EV of {studied_comp_ticker} (using avg {ratio_to_be_used}): {estimated_EV} ")
 print(f"real EV / Estimated EV : {companies_data.loc[studied_comp_ticker, 'EV'] / estimated_EV}")
 
-
-
+#estimation d'EV de l'entreprise étudiée a partir du ratio EV / revenue
 ratio_to_be_used = "EV_revenue_ratio"
 denominator_stat = "revenue"
 estimated_EV = averages.loc[ratio_to_be_used] * companies_data.loc[studied_comp_ticker, denominator_stat]
@@ -104,7 +65,7 @@ print(f"\nReal EV of {studied_comp_ticker}: {companies_data.loc[studied_comp_tic
 print(f"Estimated EV of {studied_comp_ticker} (using avg {ratio_to_be_used}): {estimated_EV} ")
 print(f"real EV / Estimated EV : {companies_data.loc[studied_comp_ticker, 'EV'] / estimated_EV}")
 
-
+#estimation d'EV de l'entreprise étudiée a partir du ratio EV / gross_profit
 ratio_to_be_used = "EV_gross_profit_ratio"
 denominator_stat = "gross_profit"
 estimated_EV = averages.loc[ratio_to_be_used] * companies_data.loc[studied_comp_ticker, denominator_stat]
@@ -112,3 +73,38 @@ estimated_EV = averages.loc[ratio_to_be_used] * companies_data.loc[studied_comp_
 print(f"\nReal EV of {studied_comp_ticker}: {companies_data.loc[studied_comp_ticker, 'EV']}")
 print(f"Estimated EV of {studied_comp_ticker} (using avg {ratio_to_be_used}): {estimated_EV} ")
 print(f"real EV / Estimated EV : {companies_data.loc[studied_comp_ticker, 'EV'] / estimated_EV}")
+
+
+
+
+# Estimation using linear regression
+stat_used = "gross_profit"
+ax = companies_data.plot.scatter(stat_used, "EV", title=f"Scatter plot of EV and {stat_used}")
+
+# Annotate each data point
+for i, txt in enumerate(companies_data.index):
+   ax.annotate(txt, (companies_data[stat_used].iat[i]+0.05, companies_data["EV"].iat[i]))
+
+
+
+X_single_var = companies_data[companies_data.index != studied_comp_ticker][[stat_used]].reset_index(drop=True) #single variable
+# X_multi_var = companies_data[companies_data.index != studied_comp_ticker][["EBIT", "revenue", "gross_profit"]].reset_index(drop=True) #multi variable
+Y = companies_data[companies_data.index != studied_comp_ticker]["EV"].reset_index(drop=True)
+studied_comp_row_single_var = companies_data[companies_data.index == studied_comp_ticker][[stat_used]].reset_index(drop=True) #single variable
+# studied_comp_row_multi_var = companies_data[companies_data.index == studied_comp_ticker][["EBIT", "revenue", "gross_profit"]].reset_index(drop=True) #multi variable
+
+linear_regressor = LinearRegression()
+linear_regressor.fit(X_single_var, Y) #single variable
+# linear_regressor.fit(X_multi_var, Y) #multi variable
+print(f"\nCoefs ligne de regression :\na={round(linear_regressor.coef_[0], 4)}\nintercept={round(linear_regressor.intercept_, 4)}\n") #single variable
+# print(f"\n\nCoefs ligne de regression :\ncoefs={linear_regressor.coef_, 4}\nnintercept={round(linear_regressor.intercept_, 4)}\n") #multi variable
+estimated_EV = linear_regressor.predict(studied_comp_row_single_var)[0] #single variable
+# estimated_EV = linear_regressor.predict(studied_comp_row_multi_var)[0] #multi variable
+
+print(f"Real EV of {studied_comp_ticker}: {companies_data.loc[studied_comp_ticker, 'EV']}")
+print(f"Estimated EV of {studied_comp_ticker} with regression model (using {stat_used}): {estimated_EV} ") #single variable
+# print(f"Estimated EV of {studied_comp_ticker} with regression model (using all stats): {estimated_EV} ") #multi variable
+print(f"real EV / Estimated EV : {companies_data.loc[studied_comp_ticker, 'EV'] / estimated_EV}\n")
+
+plt.plot(X_single_var, X_single_var*linear_regressor.coef_ + linear_regressor.intercept_, color='red') #single variable
+plt.show()
--- demo/recuperation_donnees.py
+++ demo/recuperation_donnees.py
@@ -8,10 +8,13 @@ os.chdir(sys.path[0])
 
 
 def get_financial_data(ticker_list):
-    companies_data = {}
-    for ticker in ticker_list:
-        financial_data = {}
+    cached_data, remaining_tickers = get_from_cache(ticker_list)
 
+    companies_data = {}
+    for ticker in remaining_tickers:
+        print(f"Recuperation de {ticker} sur yahoo finance")
+        financial_data = {}
+        
         statistics = si.get_stats_valuation(ticker)
         statistics = statistics.set_index(0)
         # statistics2 = si.get_stats(ticker)
@@ -21,19 +24,61 @@ def get_financial_data(ticker_list):
         ev = statistics.loc["Enterprise Value 3"].tolist()[0]
         if(type(ev) == str):
             ev = float(ev[:-1])# pour enlever le 'B' à la fin du string (indique que le chiffre est en milliard, il faudra faire attention que l'unité soit la meme partout)
-        financial_data["EV"] = ev#*(10**9)
+        financial_data["EV"] = ev*(10**3)
         
-        financial_data["EBIT"] = quarterly_income_statement_df.loc["ebit"][0]# [0] pour date la plus recente
+        financial_data["EBIT"] = quarterly_income_statement_df.loc["ebit"][0]/(10**6)# [0] pour date la plus recente
 
-        financial_data["revenue"] = quarterly_income_statement_df.loc["totalRevenue"][0]
+        financial_data["revenue"] = quarterly_income_statement_df.loc["totalRevenue"][0]/(10**6)
 
-        financial_data["gross_profit"] = quarterly_income_statement_df.loc["grossProfit"][0]
+        financial_data["gross_profit"] = quarterly_income_statement_df.loc["grossProfit"][0]/(10**6)
 
         companies_data[ticker] = financial_data
+    
+    companies_data = pd.DataFrame.from_dict(companies_data, orient='index')
+    companies_data = pd.concat([cached_data, companies_data])
+    save_to_disk(companies_data)
+    return companies_data
 
-    return pd.DataFrame.from_dict(companies_data, orient='index')
 
-# ticker_list = ['F', 'VWAGY', 'SZKMY', 'NSANY', 'HMC', 'TM', 'STLA']
-# df = get_financial_data(ticker_list)
-# print(df.info())
-# print(df)
+def save_to_disk(companies_data):
+    companies_data = companies_data.reset_index()
+    companies_data.rename(columns={'index':'ticker'}, inplace=True)
+    if(not os.path.exists("cache/")):
+        os.mkdir("cache")
+    elif (os.path.isfile("cache/companies_data.csv")):
+        saved_data = pd.read_csv("cache/companies_data.csv")
+        companies_data = pd.concat([companies_data, saved_data]).drop_duplicates()
+    
+    companies_data.to_csv("cache/companies_data.csv", index=False)
+
+def get_from_cache(ticker_list, allow_nan=False):
+    saved_data = None
+    remaining_tickers = ticker_list.copy()
+    if (os.path.isfile("cache/companies_data.csv")):
+        saved_data = pd.read_csv("cache/companies_data.csv")
+        saved_data = saved_data[saved_data['ticker'].isin(ticker_list)]
+        if(not allow_nan):
+            saved_data.dropna(axis='rows', inplace=True)
+        
+        saved_data_tickers = saved_data["ticker"].tolist()
+        remaining_tickers = [ticker for ticker in ticker_list if ticker not in saved_data_tickers]
+        saved_data.set_index("ticker", inplace = True)
+    return saved_data, remaining_tickers
+
+
+def calcul_ratios(companies_data):
+    companies_data["EV_EBIT_ratio"] = companies_data["EV"] / companies_data["EBIT"]
+    companies_data["EV_revenue_ratio"] = companies_data["EV"] / companies_data["revenue"]
+    companies_data["EV_gross_profit_ratio"] = companies_data["EV"] / companies_data["gross_profit"]
+    
+    return companies_data
+
+
+ticker_list = ['F', 'VWAGY', 'SZKMY', 'FB']
+df = get_financial_data(ticker_list)
+print(df)
+
+# ticker_list = ['F', 'VWAGY', 'SZKMY', 'FB']
+# data, remaining_tickers = get_from_cache(ticker_list)
+# print(data)
+# print(remaining_tickers)
